90b986ea8df12333abf19de8b7df5255047e0938
Nikema Prophet <nikema@gmail.com>
2019-06-14 09:25:27 -0700

    finished objects-arrays.js up to ADVANCED Array Methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -10,33 +10,58 @@ class Dinosaur {
     this.name = props.name
     this.diet = props.diet
     this.weight = props.weight
+    this.length = props.length
     this.period = props.period
   }
 }
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = new Dinosaur ({
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg', 
+  length: '12m',
+  period: 'Late Cretaceous'
+})
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = new Dinosaur ({
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg', 
+  length: '9m',
+  period: 'Late Jurassic'
+})
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = new Dinosaur ({
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg', 
+  length: '1.8m',
+  period: 'Late Cretaceous'
+})
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+tyrannosaurus.roar = () => "RAWERSRARARWERSARARARRRR!" 
+
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -57,21 +82,22 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map(graduate => graduate.university).sort()
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+
 
 The resulting contact information should have a space between the first name and the email information like this: 
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(graduate => `${graduate.first_name} ${graduate.email}`)
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = universities.filter(university => university.includes('Uni'))
 console.log(uni);
 
 
